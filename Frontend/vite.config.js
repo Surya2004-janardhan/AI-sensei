a692ac3f792b5eee86ca471cba06de37e966c7d2
SuryaJanardhanGS <surya@growstack.biz>
2025-08-01 00:19:34 +0530

    fix : crypto error fixed with vite downgrade

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import crypto from "crypto";
+// console.log("DEBUG crypto object:", crypto);
 
 // https://vite.dev/config/
 export default defineConfig({

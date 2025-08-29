e8983e96a647f3cb7d43ead4d34451b41759b9f0
Surya Janardhan <chintalajanardhan2004@gmail.com>
2025-08-29 21:08:22 +0530

    fix : backend base url from local to live render link

--- Frontend/src/api/axiosInstance.js
+++ Frontend/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api", // your backend base URL
+  baseURL: "https://ai-sensei-lej2.onrender.com", // your backend base URL
+  // "http://localhost:3000/api"
 });
 
 axiosInstance.interceptors.request.use((config) => {
